90a648fc6e0e1db7d3bdbed21d4acbc176774dcd
kasemsaad <kasemsaad756@gmail.com>
2024-06-03 21:19:49 +0300

    sidbar_medium

--- src/layout/layoutComp/layoutComp.jsx
+++ src/layout/layoutComp/layoutComp.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Header from '../../common/header/header';
 import { Outlet } from 'react-router-dom';
 import AccountSetting from '../../dashboard/Account-settting/AccountSetting';
-import SidebarFullscreen from '../../common/sidebar/structure';
+import SidebarFullscreen from '../../common/sidebar/sidbarFullscreen';
+import Sidmedscreen from '../../common/sidebar/sidmedscreen';
+
 import "./style.css"
 
 function LayoutComp() {
@@ -14,23 +16,24 @@ function LayoutComp() {
         border: "1px",
       }}>
         < Header />
-        <div className='home' style={{height: 800 }} >
+        <div className='home'>
           <div className='block row ' dir='rtl' >
-            <div className='col-md-3 me-5' dir="rtl">
+            <div className='col-md-3 me-5' dir="rtl"   style={{ paddingTop:"110px" }}>
               <SidebarFullscreen />
+              <Sidmedscreen />
               </div>
-                <div className='form col-md-9 '>
+                <div className='form col-md-9   '>
                    <div
                       style={{
-                        maxWidth: "100%",
+                        maxWidth: "96%",
                         color: "white",
-                        backgroundColor: "#0E0A43",
+                        backgroundColor: "red",
+                        height:"5000px",
                         borderRadius: "10px",
                         position: "relative",
-                        top: "-15px",
                         overflow: "auto",
                         }}>
-                      <Outlet />
+                      {/* <Outlet /> */}
                    </div>
                 </div>
             </div>
