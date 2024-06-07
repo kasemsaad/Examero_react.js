93acafeebc26477448bf8e3cc6c9360b08adebf7
kasemsaad <kasemsaad756@gmail.com>
2024-06-07 20:47:24 +0300

    create section 1,2,3

--- src/websit/Home/home.jsx
+++ src/websit/Home/home.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import './HomeStyle.css'; // Import your CSS file if needed
-import douts from "../../assets/image/home/Group 54.svg";
-
+// import douts from "../../assets/image/home/Group 54.svg";
+import MyButton from "../../common/Button/Button";
+import right from"../../assets/icons/Home/mdi_success-circle-outline.svg"
+import fluent from"../../assets/icons/Home/fluent-mdl2_commitments.svg"
+import lucide_git from"../../assets/icons/Home/lucide_git-pull-request-create-arrow.svg"
+import ph_target from"../../assets/icons/Home/ph_target-bold.svg"
 function Home() {
     return (
         <>
@@ -20,11 +24,7 @@ function Home() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="d-flex align-items-center justify-content-between" style={{ width: "100vw" }}>
-<<<<<<< HEAD
                         <div className="collapse navbar-collapse kasem" id="navbarNav" style={{ width: "80vw" }}>
-=======
-                        <div className="collapse navbar-collapse" id="navbarNav" style={{ width: "80vw" }}>
->>>>>>> 47a72796114aeff81615df5bd1af97644a4faa8e
                             <div className="classnav">
                                 <ul className="navbar-nav">
                                     <li className="nav-item">
@@ -48,8 +48,9 @@ function Home() {
                                 </ul>
                             </div>
                             <div>
-                                <button className="btn mx-2 py-0" style={{ height: "1.8rem", color: "white", backgroundColor: "#4941A6" }} to="/Home">إنشاء حساب</button>
-                                <button className="btn mx-2 py-0" style={{ height: "1.8rem", border: "2px solid #4941A6" }} to="/Home">تسجيل الدخول</button>
+                                <MyButton className="btn mx-2 py-0" style={{ height: "1.8rem", color: "white", backgroundColor: "#4941A6"  }} to={"/Home"} content={"انشاء حساب"}/>
+                                <MyButton  className="btn mx-2 py-0" style={{ height: "1.8rem", border: "2px solid #4941A6" }} to={"/Home"} content={"تسجيل الدخول"}/>
+                                
                             </div>
                         </div>
                     </div>
@@ -102,37 +103,53 @@ function Home() {
             </Section2>
             {/* -----------endsection2--------- */}
             {/* -----------section3--------- */}
-            <Section3 className="Section3 " >
-                <DivSection3 className="container DivSection3 p-0 m-1 text-center ">
+            <Section3 className="Section3 mt-5" >
+                <DivSection3 className="container DivSection3 p-0 pb-4 text-center ">
                 <h3 className="Bold"><span style={{ color: "#4941A6" }}>هدفنا</span></h3><br />
-                <p className=" fs-5" dir="rtl">هدفنا في موقع <span>'Examero'</span> هو تقديم دعم شامل للمعلمين. نسعى جاهدين لتوفير أدوات وموارد تساعدهم</p>
+                <p className=" fs-5 pt-3  " dir="rtl">هدفنا في موقع <span>'Examero'</span> هو تقديم دعم شامل للمعلمين. نسعى جاهدين لتوفير أدوات وموارد تساعدهم</p>
                 <p className="fs-6" dir="rtl">نحن نؤمن بأهمية دور المعلمين في بناء مستقبل مشرق للتعليم ونسعى لدعمهم في تحقيق هذا الهدف.</p>                   
-                <div className="row cards">
-                    <div className="col-md-2 mx-4  my-2 bg-danger">
-                       <div className="text-end">
-                        <h1>fdsdf</h1>
+                <div className="row cards p-0 m-0 ">
+                    <div className="card  col-md-2 p-0 m-3 shadow bg-white " >
+                        <div className="parentCircleIcon  p-0">
+                       <div className="circleicone">
+                        <img style={{width:"25px", paddingRight:"4px"}} src={right} alt="right" />
                        </div>
-                       <div>
-                        <p>غايتنا هي تقديم مساعدة حقيقية للمعلمين وتحقيق النجاح في التعليم.</p>
+                        </div>
+                       <div className="p-3">
+                        <p><span style={{ color: "#A63131" }}>غايتنا</span> هي تقديم مساعدة حقيقية للمعلمين وتحقيق النجاح في التعليم.</p>
                        </div>
                     </div>
-                    <div className="col-md-2  mx-4 my-2 bg-danger">
-                        <h2>df</h2>
-                        <h2>df</h2>
-                        <h2>df</h2>
+                    <div className="card  col-md-2 p-0 m-3 shadow bg-white " >
+                        <div className="parentCircleIcon  p-0">
+                       <div className="circleicone">
+                        <img style={{width:"25px", paddingRight:"4px"}} src={fluent} alt="right" />
+                       </div>
+                        </div>
+                       <div className="p-3">
+                        <p>نحن <span style={{ color: "#A63131" }}>ملتزمون</span> بتوفير أدوات مبتكرة لتقديم تجربة تعليمية 
+                        ممتازة.</p>
+                       </div>
                     </div>
-                    <div className="col-md-2  mx-4 my-2 bg-danger">
-                        <h2>df</h2>
-                        <h2>df</h2>
-                        <h2>df</h2>
+                    <div className="card  col-md-2 p-0 m-3 shadow bg-white " >
+                        <div className="parentCircleIcon  p-0">
+                       <div className="circleicone">
+                        <img style={{width:"25px", paddingRight:"4px"}} src={lucide_git} alt="right" />
+                       </div>
+                        </div>
+                       <div className="p-3">
+                        <p> <span style={{ color: "#A63131" }}>نسعى </span>لتوفير منصة تسهل عليك إنشاء أسئلة امتحانية بسرعة ودقة.</p>
+                       </div>
                     </div>
-                    <div className="col-md-2  mx-4 my-2 bg-danger">
-                        <h2>df</h2>
-                        <h2>df</h2>
-                        <h2>df</h2>
+                    <div className="card  col-md-2 p-0 m-3 shadow bg-white " >
+                        <div className="parentCircleIcon  p-0">
+                       <div className="circleicone">
+                        <img style={{width:"25px", paddingRight:"4px"}} src={ph_target} alt="right" />
+                       </div>
+                        </div>
+                       <div className="p-3">
+                        <p><span style={{ color: "#A63131" }}>هدفنا </span> هو دعم المعلمين في إعداد الامتحانات بطريقة ميسرة ومؤثرة.</p>
+                       </div>
                     </div>
-                   
-                 
                 </div>
                 </DivSection3>
             </Section3>
