d30ca05259e0c306757bf0a3d1efe6ddac2bd83f
saraalyan <alyansara817@gmail.com>
2024-06-12 15:28:01 +0400

    update2

--- src/App.js
+++ src/App.js
@@ -24,12 +24,13 @@ function App(props) {
         <Routes>
 
           <Route path="/" element={<Home />} />
-           <Route path="/ct" element={<CreateTechAcc/>} />
           <Route path="/cs" element={<CreateStudentAcc/>} /> 
+            <Route path="/ct" element={<CreateTechAcc/>} />
+     
           <Route path="/l1" element={<Login1/>} />
-          {/* <Route path="/reset1" element={<Reset_page1/>} />
+           <Route path="/newp" element={<New_pass/>} /> 
           <Route path="/reset2" element={<Reset_code_page/>} />
-          <Route path="/newp" element={<New_pass/>} /> */}
+          <Route path="/reset1" element={<Reset_page1/>} /> 
 
           <Route path="/teacher/" element={<LayoutComp />} />
           <Route path="/dashboard/" element={<LayoutComp />}>
