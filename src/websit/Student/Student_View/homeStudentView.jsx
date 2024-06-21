725d2f067138ad042066b9370706f843fb68192f
kasemsaad <kasemsaad756@gmail.com>
2024-06-21 19:23:28 +0300

    add_update_note

--- src/websit/Student/Student_View/homeStudentView.jsx
+++ src/websit/Student/Student_View/homeStudentView.jsx
@@ -11,14 +11,18 @@ import request from '../../../utlis/axios_utils_websit.jsx';
 import '../Student_View/homeStudentView.css';
 import "../../../dashboard/Home_Dashboard/home_dashboard.css";
 import "./DeleteElement.css";
-// import { useNavigate } from 'react-router-dom';
+import "./AddNewUser.css";
 
+let useId;
+function onSelect(id) {
+  useId = id
+}
+function HomeStudentview(props) {
+  const getToken = () => { return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3N0dWRlbnRzL2xvZ2luIiwiaWF0IjoxNzE4OTgzOTA2LCJleHAiOjE3MTg5ODc1MDYsIm5iZiI6MTcxODk4MzkwNiwianRpIjoiVEpBd05LTUZPZHdXUUJ5QSIsInN1YiI6IjExIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.QTdGDoolv59gN2QuiJKCsMMZ7L4E1I-6I6gUdwCo7Qg"; };
+  const layoutBackground = useSelector((state) => state.dark.lay);
 
-function HomeStudentview(props) { 
-    const getToken = () => {    return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3N0dWRlbnRzL2xvZ2luIiwiaWF0IjoxNzE4ODgyOTk0LCJleHAiOjE3MTg4ODY1OTQsIm5iZiI6MTcxODg4Mjk5NCwianRpIjoicVViZnJJU3c2QktHQTl3dSIsInN1YiI6IjExIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.KU1BD5lGEcaXVQjpOGChcZnow28FBschnvUXvUJATfo";  };
-    const layoutBackground = useSelector((state) => state.dark.lay);
-    const [allNotes, setAllNotes] = useState(null);
-
+  //////////////////////////Get All Note///////////////////////////////////////////////////
+  const [allNotes, setAllNotes] = useState("");
   useEffect(() => {
     request({
       url: '/students/notes',
@@ -34,32 +38,168 @@ function HomeStudentview(props) {
         console.error("Error fetching notes data:", error);
       });
   }, []);
-  /////////////////////////////////////////////////////////////////////
-  let idDelete;
-  function onSelect(id) {
-    idDelete = id
-  }
-  //////////////////////////deleteNote///////////////////////////////////////////////////
+  //////////////////////////End Get All Note///////////////////////////////////////////////////
+  //////////////////////////Delete Note///////////////////////////////////////////////////
   const deleteNote = (id) => {
     request({
       url: `/students/notes/${id}`,
       method: 'delete',
       headers: {
-      Authorization: `Bearer ${getToken()}`
+        Authorization: `Bearer ${getToken()}`
       }
     })
-      .try(response => {
+      .then(response => {
         console.log('Note deleted successfully');
       })
       .catch(error => {
-        console.error('Error deleting note:', error);
+        console.error('Error deleting note:');
       });
   };
+  //////////////////////////End Delete Note///////////////////////////////////////////////////
+  //////////////////////////add Note///////////////////////////////////////////////////
+  const [address, setAddress] = useState('');
+  const [note, setNote] = useState('');
+  const [addressValidationMessage, setAddressValidationMessage] = useState('');
+  const [noteValidationMessage, setNoteValidationMessage] = useState('');
+  const handleAddressChange = (event) => {
+    const value = event.target.value;
+
+    if (value.trim() === '') {
+      setAddressValidationMessage('لا يجب ان يكون فارغ');
+    } else if (value.length > 15) {
+      setAddressValidationMessage('العنوان لايزيد عن 15 حرف');
+    } else if (!/^[\u0600-\u06FF\sA-Za-z]+$/.test(value)) {
+      setAddressValidationMessage('يجب ان يكون نص');
+    } else {
+      setAddressValidationMessage('');
+    }
+    setAddress(value);
+  };
+
+  const handleNoteChange = (event) => {
+    const value = event.target.value;
+
+    if (value.trim() === '') {
+      setNoteValidationMessage('لا يجب ان يكون فارغ');
+    } else if (value.length > 50) { // Example max length for note
+      setNoteValidationMessage('الملحوظه لاتزيد عن 50 حرف');
+    } else if (!/^[\u0600-\u06FF\sA-Za-z]+$/.test(value)) {
+      setNoteValidationMessage('يجب ان يكون نص');
+    } else {
+      setNoteValidationMessage('');
+    }
+    setNote(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      address: address,
+      note: note
+    };
+
+    request({
+      url: 'students/notes', method: 'post', data,
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    })
+      .then(response => {
+        console.log('Note added successfully:');
+        const modalElement = document.getElementById('addManagerModal');
+        modalElement.style.display = "none"
+      })
+      .catch(error => {
+        console.error('Error adding note:');
+      });
+  };
+
+  //////////////////////////End add Note///////////////////////////////////////////////////
+  //////////////////////////update Note///////////////////////////////////////////////////
+
+  const [getDataUpdateAddress , setgetDataUpdateAddress]=useState("");
+  const [getDataUpdateNote , setgetDataUpdateNote]=useState("");
+  const handleGetUpdate = (id) => {
+    request({
+      url: `students/notes/${id}`, method: 'get',
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    })
+      .then(response => {
+        setgetDataUpdateNote(response.data.data.note);
+        setgetDataUpdateAddress(response.data.data.address);
+
+              })
+      .catch(error => {
+        console.error('Error get note:');
+      });
+  };
+  ////////////////////////////////////add update//////////////////////////////////////////////////////////////
+  const [DataAfterUpdateAddress, setDataAfterUpdateAddress] = useState("");
+  const [DataAfterUpdateNote, setDataAfterUpdateNote] = useState("");
+  const [addressUpdateValidationMessage, setAddressUpdateValidationMessage] = useState('');
+  const [noteUpdateValidationMessage, setNoteUpdateValidationMessage] = useState('');
+
+  const handleAddressChangeUpdate = (event) => {
+    const value = event.target.value;
+
+    if (value.trim() === '') {
+      setAddressUpdateValidationMessage('لا يجب ان يكون فارغ');
+    } else if (value.length > 15) { // Example max length for address
+      setAddressUpdateValidationMessage('العنوان لايزيد عن 15 حرف');
+    } else if (!/^[\u0600-\u06FF\sA-Za-z]+$/.test(value)) {
+      setAddressUpdateValidationMessage('يجب ان يكون نص');
+    } else {
+      setAddressValidationMessage('');
+    }
+    setDataAfterUpdateAddress(value)
+  };
+
+  const handleNoteChangeUpdate = (event) => {
+    const value = event.target.value;
+
+    // Validate note input
+    if (value.trim() === '') {
+      setNoteUpdateValidationMessage('لا يجب ان يكون فارغ');
+    } else if (value.length > 50) { // Example max length for note
+      setNoteUpdateValidationMessage('الملحوظه لاتزيد عن 50 حرف');
+    } else if (!/^[\u0600-\u06FF\sA-Za-z]+$/.test(value)) {
+      setNoteUpdateValidationMessage('يجب ان يكون نص');
+    } else {
+      setNoteUpdateValidationMessage('');
+
+    }
+    setDataAfterUpdateNote(value)
+  };
+  const handleSubmitUpdate = (event) => {
+
+    event.preventDefault();
+    const data = {
+      address: DataAfterUpdateAddress,
+      note: DataAfterUpdateNote
+    };
+
+    request({
+      url: `students/notes/${useId}`, method: 'post', data,
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    })
+      .then(response => {
+        console.log('Note update successfully:');
+        const modalElement = document.getElementById('UpdateManagerModal');
+        modalElement.style.display = "none"
+      })
+      .catch(error => {
+        console.error('Error update note:');
+      });
+  };
+  //////////////////////////End add Note///////////////////////////////////////////////////
 
   if (!allNotes) {
     return <div>Loading...</div>;
   }
-
   return (
     <>
       <div className="container py-5 mb-2 d-flex align-items-center justify-content-center flex-column">
@@ -132,7 +272,7 @@ function HomeStudentview(props) {
                           <div className="wraber_delete d-flex justify-content-center align-items-center" style={{ backgroundColor: "#1D195D", width: "2vw", height: "2vw", borderRadius: "8px" }}>
                             <img src={delet} alt="Delete" width="17px" height="17px" data-bs-toggle="modal" data-bs-target="#deleteElementModal" onClick={() => onSelect(id)} />
                           </div>
-                          <div className="wraber_delete d-flex justify-content-center align-items-center" style={{ backgroundColor: "#4941A6", width: "2vw", height: "2vw", borderRadius: "8px", boxShadow: 'rgba(0, 0, 0, 0.75) -2px 3px 5px 0px' }}>
+                          <div data-bs-toggle="modal" data-bs-target="#UpdateManagerModal" className="wraber_delete d-flex justify-content-center align-items-center" onClick={() =>{onSelect(id); handleGetUpdate(id);} } style={{ backgroundColor: "#4941A6", width: "2vw", height: "2vw", borderRadius: "8px", boxShadow: 'rgba(0, 0, 0, 0.75) -2px 3px 5px 0px' }}>
                             <img src={edit} alt="Edit" />
                           </div>
                         </div>
@@ -142,7 +282,13 @@ function HomeStudentview(props) {
                 )}
 
                 <div style={{ textAlign: "center", display: 'flex', justifyContent: "center", marginTop: "10px" }}>
-                  <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="submit" className="btn mx-2" style={{ backgroundColor: "#FE4F60", color: '#FFFFFF' }}>
+                  <button
+
+                    type="button"
+                    className="btn mb-2"
+                    data-bs-toggle="modal"
+                    data-bs-target="#addManagerModal"
+                    style={{ backgroundColor: "#FE4F60", color: '#FFFFFF' }}>
                     <span style={{ marginLeft: "10px" }}>
                       <img src={plus} alt="Plus Icon" />
                     </span>
@@ -154,7 +300,9 @@ function HomeStudentview(props) {
           </div>
         </div>
       </div>
+      {/* ////////////////////////////////////////////modal/////////////////////////////////////////////////////////    */}
       <div
+
         className="modal fade DElementFade "
         id="deleteElementModal"
         tabIndex="-1"
@@ -181,7 +329,7 @@ function HomeStudentview(props) {
                   type="button"
                   className="btn btn-danger cancel-btn DElementSave mx-1"
                   data-bs-dismiss="modal"
-                  onClick={() => deleteNote(idDelete)}
+                  onClick={() => deleteNote(useId)}
                 >
                   نعم
                 </button>
@@ -190,6 +338,128 @@ function HomeStudentview(props) {
           </div>
         </div>
       </div>
+
+      {/* ////////////////////////////////////////////endmodals/////////////////////////////////////////////////////////    */}
+      {/* ////////////////////////////////////////////add modals/////////////////////////////////////////////////////////    */}
+      <div
+        className="modal fade managerFade"
+        id="addManagerModal"
+        tabIndex="-1"
+        aria-labelledby="addManagerModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered managergDialog">
+          <div className="modal-content managerContent">
+            <div className="modal-header managerHeader">
+              <h5 className="modal-title managerTitle" >
+                إضافة ملحوظة
+              </h5>
+              <button type="button" className="btn-close kh" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body managerBody">
+              <form className="modal-body managerForm" onSubmit={handleSubmit}>
+                <div className="parent1">
+                  <div className="child1 col-lg-5">
+                    <div className="form-group managerFGroup">
+                      <label htmlFor="lastName">العنوان</label>
+                      <input
+                        type="text"
+                        className="form-control managerControl"
+                        id="lastName"
+                        placeholder={`أدخل العنوان `}
+                        value={address}
+                        onChange={handleAddressChange}
+                      />
+                      {addressValidationMessage && <p style={{ color: 'red' }}>{addressValidationMessage}</p>}
+
+                      <span className="form-text text-muted">
+
+                      </span>
+                    </div>
+                    <div className="form-group managerFGroup">
+                      <label htmlFor="">الملحوظة</label>
+                      <input
+                        type="text"
+                        className="form-control managerControl"
+                        id="lastName"
+                        placeholder="أدخل الملحوظة"
+                        value={note}
+                        onChange={handleNoteChange} />
+                      {noteValidationMessage && <p style={{ color: 'red' }}>{noteValidationMessage}</p>}
+
+                    </div>
+
+                    <div className="modal-footer managerFooter ms-4 ">
+                      <button
+                        type="button"
+                        className="btn canceled managerCancel"
+                        data-bs-dismiss="modal"
+                        id="firstbutt"
+                      >
+                        إلغاء
+                      </button>
+                      <button type="submit" className="btn save managerSave">إضافة</button>
+                    </div>
+                  </div>
+
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* //////////////////////////////////////////// end add modals/////////////////////////////////////////////////////////    */}
+      {/* ////////////////////////////////////////////Update modals/////////////////////////////////////////////////////////    */}
+      <div className="modal fade managerFade" id="UpdateManagerModal" tabIndex="-1" aria-labelledby="addManagerModalLabel" aria-hidden="true" >
+        <div className="modal-dialog modal-dialog-centered managergDialog">
+          <div className="modal-content managerContent">
+            <div className="modal-header managerHeader">
+              <h5 className="modal-title managerTitle">تعديل الملحوظة</h5>
+              <button type="button" className="btn-close kh" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body managerBody">
+              <form className="modal-body managerForm" onSubmit={handleSubmitUpdate}>
+                <div className="parent1">
+                  <div className="child1 col-lg-5">
+                    <div className="form-group managerFGroup">
+                      <label htmlFor="address">العنوان</label>
+                      <input
+                        type="text"
+                        className="form-control managerControl"
+                        id="address"
+                        placeholder="أدخل العنوان"
+                        value={getDataUpdateAddress}
+                        onChange={handleAddressChangeUpdate}
+                      />
+                      {addressUpdateValidationMessage && <p style={{ color: 'red' }}>{addressUpdateValidationMessage}</p>}
+                    </div>
+                    <div className="form-group managerFGroup">
+                      <label htmlFor="note">الملحوظة</label>
+                      <input
+                        type="text"
+                        className="form-control managerControl"
+                        id="note"
+                        placeholder="أدخل الملحوظة"
+                        value={getDataUpdateNote}
+
+                        onChange={handleNoteChangeUpdate}
+                      />
+                      {noteUpdateValidationMessage && <p style={{ color: 'red' }}>{noteUpdateValidationMessage}</p>}
+                    </div>
+                  </div>
+                </div>
+                <div className="modal-footer managerFooter ms-4 pt-3" >
+                  <button type="button" className="btn canceled managerCancel" data-bs-dismiss="modal" id="firstbutt">
+                    إلغاء
+                  </button>
+                  <button type="submit" className="btn save managerSave">تعديل</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* //////////////////////////////////////////// end Update modals/////////////////////////////////////////////////////////    */}
 
     </>
   );
