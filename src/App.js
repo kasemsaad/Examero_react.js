5f619a8283dcff4b36322b132cf5ae995fd0c42d
mostafa esam <mostafaesam3001@gmail.com>
2024-06-22 18:00:53 +0300

    resolve app.h=js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fontsource/cairo"; // Defaults to weight 400
 // import Header from './common/header/header';
 import LayoutComp from './layout/layoutComp/layoutComp';
 import AccountSetting from './dashboard/Account-settting/AccountSetting';
-import LayoutComp from "./layout/layoutComp/layoutComp";
-import AccountSetting from "./dashboard/Account-settting/AccountSetting";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./websit/Home/home.jsx";
-import Notification from "./dashboard/Notification/Notification";
-import PuttingQuestions from "./dashboard/PuttingQussions/ForClasses/PuttingQuestions.jsx";
-import PuttingQForMab7as from "./dashboard/PuttingQussions/ForMab7s/PuttingQForMabhas.jsx";
-import "@fontsource/cairo"; // Defaults to weight 400
-import Home_dashboard from './dashboard/Home_Dashboard/Home_dashboard.jsx';
-import CreateTechAcc from './websit/register and login/create_taech_acc/create_teach_acc.jsx';
+import CreateExam from './websit/Student/createExam/createExam.jsx';
+import HomeStudentView from './websit/Student/Student_View/homeStudentView.jsx'
 import CreateStudentAcc from'./websit/register and login/create_stud_acc/create_stud_acc.jsx';
-import Reset_page1 from './websit/register and login/resetpage/resetpage.jsx'
-import Reset_code_page from './websit/register and login/reset_password/reset_password.jsx';
 import Login1 from'./websit/register and login/login/login2.jsx'
-import New_pass from './websit/register and login/new_pass/new_pass.jsx'
 import Login from'./websit/register and login/login/login1.jsx'
 import CreateTechAcc from './websit/register and login/create_taech_acc/create_teach_acc.jsx';
 import Reset_page1 from './websit/register and login/resetpage/resetpage_stud.jsx'
 import Reset_code_page from './websit/register and login/reset_password/reset_password_stud.jsx';
+import New_pass from './websit/register and login/new_pass/new_pass_stud.jsx'
+
 import ResetPage1Teacher from './websit/register and login/resetpage/resetpage_teach.jsx'
 import ResetCodePageTech from './websit/register and login/reset_password/reset_password_tech.jsx';
 import NewPassTeach from './websit/register and login/new_pass/new_pass_tech.jsx'
@@ -34,10 +28,9 @@ import PuttingExam4 from './websit/teacher_view/PuttingExam1/PuttingExam4.jsx';
 import Certified_exam from './websit/teacher_view/PuttingExam1/Certified_exam.jsx';
 import EmailVerificationTech from './websit/register and login/EmailVerification/EmailVerificationtech.jsx';
 import EmailVerificationStud from './websit/register and login/EmailVerification/EmailVerificationStud.jsx';
-import HomeStudentView from './websit/Student/Student_View/homeStudentView.jsx'
-import CreateExam from './websit/Student/createExam/createExam.jsx';
 
 
+import Home_dashboard from './dashboard/Home_Dashboard/Home_dashboard.jsx';
 import HomeDashoardLogin from './dashboard/homeLogin/homeLogin.jsx';
 import CertificateGenerator from './dashboard/Certificate/Certificate.jsx';
 import Qbank from './dashboard/Qbank/Qbank.jsx';
@@ -46,20 +39,22 @@ import ProtectedRoute from './dashboard/protectedRouteDashboard.jsx/protectedRou
 import Plans from './dashboard/Plans/Plans.jsx';
 import PlansStudent from './dashboard/Plans/PlansStudent/PlansStudent.jsx';
 import PlansTeacher from './dashboard/Plans/PlansTeacher/PlansTeacher.jsx';
-
+import Notification from "./dashboard/Notification/Notification";
+import PuttingQuestions from "./dashboard/PuttingQussions/ForClasses/PuttingQuestions.jsx";
+import PuttingQForMab7as from "./dashboard/PuttingQussions/ForMab7s/PuttingQForMabhas.jsx";
 
 function App(props) {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
           <Route path="/verify-account" element={<EmailVerificationTech />} />
           {/* <Route path="/verify-account-student" element={<EmailVerificationStud />} /> */}
           <Route path="/CreateStudentAccount" element={<CreateStudentAcc/>} /> 
           <Route path="/CreateTecherAccount" element={<CreateTechAcc/>} />
           <Route path="/login_student" element={<Login1/>} />
           <Route path="/login_teacher" element={<Login/>} />
-        <Route path="/StudentNewPassword" element={<New_pass/>} /> 
+        {/* <Route path="/StudentNewPassword" element={<New_pass/>} />  */}
           <Route path="/StudentEnterCode" element={<Reset_code_page/>} />
           <Route path="/StudentSendEmail" element={<Reset_page1/>} /> 
 
@@ -89,7 +84,6 @@ function App(props) {
            <Route path="/teacher/" element={<LayoutComp />} >
           <Route path="Certified_exam" element={<Certified_exam/>} />
            </Route>
-           
            <Route path="login_dashboard" element={<HomeDashoardLogin />} />
         <Route path="/dashboard/" element={<LayoutComp />}>
           <Route index element={
